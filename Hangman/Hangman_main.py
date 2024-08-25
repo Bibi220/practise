a325cc82e4014d7a2171c06972d6e961d5f7c0fe
Bíborka Kocsis <biborkakocsis@gmail.com>
2024-08-25 19:03:07 +0200

    rewrite

--- Hangman/Hangman_main.py
+++ Hangman/Hangman_main.py
@@ -6,30 +6,38 @@ word_list = ["aardvark", "baboon", "camel"]
 # TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-display = ""
-placeholder = ""
-guess = input("Guess a letter: ").lower()
+
 # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 length_of_chword = len(chosen_word)
 # for a in range(0,length_of_chword):
-# placeholder = length_of_chword * "_"
-# print(placeholder)
-for b in range(length_of_chword):
-    placeholder += "_"
-print(placeholder)
+
+
+word_in_progress = length_of_chword * "_"
+word_in_progress = list(word_in_progress)
+print(''.join(word_in_progress))
+
+
+
+guess = input("Guess a letter: ").lower()
 
 for i in range(length_of_chword):
     if guess == chosen_word[i]:
-        placeholder[i] = guess
-print(placeholder)
+        word_in_progress[i] = guess
+print(''.join(word_in_progress))
+
+guess = input("Guess a letter: ").lower()
+
+for i in range(length_of_chword):
+    if guess == chosen_word[i]:
+        word_in_progress[i] = guess
+print(''.join(word_in_progress))
 
 
+guess = input("Guess a letter: ").lower()
 
-
-
-
-
-
+for i in range(length_of_chword):
+    if guess == chosen_word[i]:
+        word_in_progress[i] = guess
+print(''.join(word_in_progress))
 
 
